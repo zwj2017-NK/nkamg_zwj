71d031e3f2f90dce45742a4f51d497328e9fd130
朱文焌 <1103466626@qq.com>
2018-09-06 22:42:54 +0800

    Update change_file_and_dir_attribute.py

--- change_file_and_dir_attribute.py
+++ change_file_and_dir_attribute.py
@@ -17,12 +17,12 @@ def chmod_dir(path_list):
           print i+j+k
     for i in strpath:
       for j in strpath:
-        path_dir = "{0}/{1}/{2}".format(path_malhd,i,j)
+        path_dir = "{0}/{1}/{2}".format(path_hd,i,j)
         os.system('chmod 744 ' + path_dir)
         print i+j
     for k in strpath:
-      path_dir = "{0}/{1}".format(path_malhd, k)
-      os.system('chmod 744 ' + path_sample)
+      path_dir = "{0}/{1}".format(path_hd, k)
+      os.system('chmod 744 ' + path_dir)
       print k
 
 #更改文件目录下文件夹和文件的组属性
