6725dd5dc11bec9316a6fbc163b549f2ea540883
朱文焌 <1103466626@qq.com>
2018-09-07 06:01:56 +0800

    Add files via upload

---
+++ move_apk.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*-coding: utf-8 -*-
+
+import os
+import shutil
+import multiprocessing as mp
+
+#把apk文件从文件夹移动到三层目录下
+path_src= '/data/test/'
+path_des = '/data/malware'
+def return_path(path_des, filename):
+  return "{0}/{1}/{2}/{3}/".format(path_des, filename[0], filename[1], filename[2])
+
+def move_apk(filename):
+  if os.path.exists(return_path(path_des,filename) + filename):
+    return
+  
+  if 'download' in filename:
+    try:
+      os.rename(path_src + filename, path_src + filename.split('sha256=')[-1])
+    except Exception,e:
+      print str(e)
+      return
+    shutil.move(path_src + filename.split('sha256=')[-1], return_path(path_des, filename.split('sha256=')[-1]))
+  else:
+    shutil.move(path_src + filename, return_path(path_des, filename))
+
+def main():
+  pool = mp.Pool(32)
+  list_filename = os.listdir(path_src)
+  for time in xrange(len(list_filename) + 1):
+    pool.map(move_apk, list_filename[time * 32 : (time + 1) * 32])
+  pool.close()  
+
+if __name__ == '__main__':
+  main()  
