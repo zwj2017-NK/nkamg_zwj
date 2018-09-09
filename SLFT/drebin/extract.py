59e2061cb6d8ef62c6d8a9a6f7288f777d670b19
朱文焌 <1103466626@qq.com>
2018-09-09 21:22:39 +0800

    Update extract.py

--- SLFT/drebin/extract.py
+++ SLFT/drebin/extract.py
@@ -11,8 +11,8 @@ import psutil, datetime
 def return_path(path, suffix):
   return "{0}/{1}/{2}/{3}/".format(path, suffix[0], suffix[1], suffix[2])
 
-def return_time():
-  return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#def return_time():
+#  return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def extract():
   list_todo = []
