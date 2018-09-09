caaa5cbf430ac5e10e5018461f9a5ca3926062be
朱文焌 <1103466626@qq.com>
2018-09-09 22:51:45 +0800

    Update move_apk.py

--- move_apk.py
+++ move_apk.py
@@ -21,32 +21,34 @@ def get_sha256(x):
 def return_path(path, filename):
   return "{0}/{1}/{2}/{3}/".format(path, filename[0], filename[1], filename[2])
 
-def move_apk(filename):
-  if len(filename) < 64:
-    return
-  if 'data' in filename or 'xml' in filename:
-    return
+def move_apk(list_filename):
+  for filename in list_filename:
+    if len(filename) < 64:
+      return
+    if 'data' in filename or 'xml' in filename:
+      return
 
-  path_file = path_src + '/' + filename
-  sha256 = get_sha256(path_file)
-  if sha256 == filename:
-    os.system('mv ' + path_file + ' ' + return_path(path_des, filename))
-    print path_file
+    path_file = path_src + '/' + filename
+    sha256 = get_sha256(path_file)
+    if sha256 == filename:
+      os.system('mv ' + path_file + ' ' + return_path(path_des, filename))
+      print path_file
 
-    if os.path.exists(path_file + '.data'):
-      os.system('mv ' + path_src + '/' + filename + '.data' + ' ' + return_path(path_des, filename))
-      print path_file + '.data'
+      if os.path.exists(path_file + '.data'):
+        os.system('mv ' + path_src + '/' + filename + '.data' + ' ' + return_path(path_des, filename))
+        print path_file + '.data'
 
-    if os.path.exists(path_file + '.xml'):
-      os.system('mv ' + path_src + '/' + filename + '.xml' + ' ' + return_path(path_des, filename))
-      print path_file + '.xml'
+      if os.path.exists(path_file + '.xml'):
+        os.system('mv ' + path_src + '/' + filename + '.xml' + ' ' + return_path(path_des, filename))
+        print path_file + '.xml'
 
 def main():
-  pool = mp.Pool(32)
+  #pool = mp.Pool(32)
   list_filename = os.listdir(path_src)
-  for seq in xrange(len(list_filename) + 1):
-    pool.map(move_apk, list_filename[seq * 32 : (seq + 1) * 32])
-  pool.close()
+  #for seq in xrange(len(list_filename) + 1):
+  #  pool.map(move_apk, list_filename[seq * 32 : (seq + 1) * 32])
+  #pool.close()
+  move_apk(list_filename)
 
 if __name__ == '__main__':
   main()
