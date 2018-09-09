4c130509b129e783d1dcd037b53f9271780edc8c
朱文焌 <1103466626@qq.com>
2018-09-09 23:47:44 +0800

    Update get_feature_list.py

--- get_feature_list.py
+++ get_feature_list.py
@@ -6,9 +6,6 @@ import codecs
 import hashlib
 import multiprocessing as mp
 
-list_str = '0123456789abcdef'
-#path_test = '/media/zhuwenjun/a046836e-2436-4362-a0f1-0023db7b7ad4/malware'
-
 def check(suffix):
   list_except = []
   for path_hd in ['/data/benign', '/data/malware']:
@@ -22,7 +19,8 @@ def check(suffix):
     print path_dir
   return list_except
 
-if __name__ == '__main__':
+def main():
+  list_str = list('0123456789abcdef')
   pool = mp.Pool(32)
   list_path = []
   for first_dir in list_str:
@@ -30,7 +28,6 @@ if __name__ == '__main__':
       for third_dir in list_str:
         list_path.append(first_dir + second_dir + third_dir)
   m = len(list_path) / 32
-  
   list_task = [list_path[m * i : m * (i + 1)] for i in xrange(32)]
   result = pool.map(check, list_task)
   with open('todo_list_for_feature.txt','ab') as f:
@@ -43,4 +40,6 @@ if __name__ == '__main__':
         else:
           f.write(path_str + '\n')
   pool.close()
-
+if __name__ == '__main__':
+  main()
+  
