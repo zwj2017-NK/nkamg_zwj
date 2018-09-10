eefaed72865679a91dce4d1dbd9cf84c6bb12909
朱文焌 <1103466626@qq.com>
2018-09-10 10:01:21 +0800

    Update check_sha256.py

--- check_sha256.py
+++ check_sha256.py
@@ -16,19 +16,20 @@ def get_sha256(x):
   sha256=sh.hexdigest()
   return sha256
 
-def check(suffix):
+def check(list_suffix):
   list_except = []
-  for path_hd in ['/data/benign', '/data/malware']:
-    path_dir = "{0}/{1}/{2}/{3}/".format(path_hd, suffix[0], suffix[1], suffix[2])
-    paths = os.listdir(path_dir)
-    for item in paths:
-      if 'xml' in item or 'data' in item or 'csv' in item:
-        continue
-      if len(item) == 64:
-        sha256 = get_sha256(path_dir + item)
-        if sha256 <> item:
-          list_except.append(path_dir + item)
-    print path_dir
+  for suffix in list_suffix:
+    for path_hd in ['/data/benign', '/data/malware']:
+      path_dir = "{0}/{1}/{2}/{3}/".format(path_hd, suffix[0], suffix[1], suffix[2])
+      paths = os.listdir(path_dir)
+      for item in paths:
+        if 'xml' in item or 'data' in item or 'csv' in item:
+          continue
+        if len(item) == 64:
+          sha256 = get_sha256(path_dir + item)
+          if sha256 <> item:
+            list_except.append(path_dir + item)
+      print path_dir
   return list_except
 
 if __name__ == '__main__':
@@ -38,17 +39,17 @@ if __name__ == '__main__':
     for second_dir in list_str:
       for third_dir in list_str:
         list_path.append(first_dir + second_dir + third_dir)
-
-  for m in xrange(len(list_path) / 32 + 1):
-    result = pool.map(check, list_path[32 * m : 32 * (m + 1)])
-    with open('report_about_sample.txt','ab') as f:
-      for each in result:
-        if len(each) == 0:
+  m = len(list_path) / 32
+  list_task = [list_path[m * i : m * (i + 1)] for i in xrange(32)]
+  result = pool.map(check, list_path[32 * m : 32 * (m + 1)])
+  with open('report_about_sample.txt','ab') as f:
+    for each in result:
+      if len(each) == 0:
           continue
-        for path_str in each:
-          if len(path_str) == 0:
-            continue
-          else:
-            f.write(path_str + '\n')
+      for path_str in each:
+        if len(path_str) == 0:
+          continue
+        else:
+          f.write(path_str + '\n')
   pool.close()
 
