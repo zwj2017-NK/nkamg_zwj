cc0fe8cd5b3f9bec516e026817eb3ab7e7bc7ec4
朱文焌 <1103466626@qq.com>
2018-09-13 10:15:02 +0800

    Update get_feature_list.py

--- get_feature_list.py
+++ get_feature_list.py
@@ -6,18 +6,17 @@ import codecs
 import hashlib
 import multiprocessing as mp
 
-def check(list_suffix):
+def check(prefix):
   list_except = []
-  for suffix in list_suffix:
-    for path_hd in ['/data/benign', '/data/malware']:
-      path_dir = "{0}/{1}/{2}/{3}/".format(path_hd, suffix[0], suffix[1], suffix[2])
-      paths = os.listdir(path_dir)
-      for filename in paths:
-        if len(filename) <> 64:
-          continue
-        if os.path.exists(path_dir + filename) and not os.path.exists(path_dir + filename + '.data'):
-          list_except.append(filename)
-      print path_dir
+  for path_hd in ['/data/benign', '/data/malware']:
+    path_dir = "{0}/{1}/{2}/{3}/".format(path_hd, prefix[0], prefix[1], prefix[2])
+    paths = os.listdir(path_dir)
+    for filename in paths:
+      if len(filename) <> 64:
+        continue
+      if os.path.exists(path_dir + filename) and not os.path.exists(path_dir + filename + '.data'):
+        list_except.append(filename)
+    print path_dir
   return list_except
 
 def main():
@@ -28,19 +27,13 @@ def main():
     for second_dir in list_str:
       for third_dir in list_str:
         list_path.append(first_dir + second_dir + third_dir)
-  m = len(list_path) / 32
-  list_task = [list_path[m * i : m * (i + 1)] for i in xrange(32)]
-  result = pool.map(check, list_task)
-  #print result
+  result = pool.map(check, list_path)
   with open('todo_list_for_feature.txt','ab') as f:
     for each in result:
       if len(each) == 0:
         continue
-      for path_str in each:
-        if len(path_str) == 0:
-          continue
-        else:
-          f.write(path_str + '\n')
+      else:
+        f.write(each + '\n')
   pool.close()
   
 if __name__ == '__main__':
